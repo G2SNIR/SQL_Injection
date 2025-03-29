2f4e589049892d971c946a0f7f682513ddcc922d
g2.iris.lla@gmail.com <Gatan>
2025-03-29 17:04:53 +0100

    Gestion des listes ul en CSS

--- Vue/page_mvc.php
+++ Vue/page_mvc.php
@@ -7,4 +7,12 @@
     <li>the view, the interface that presents information to and accepts it from the user</li>
     <li>the controller, the software linking the two.</li>
 </ul>
-<p>Traditionally used for desktop graphical user interfaces (GUIs), this pattern became popular for designing web applications.[4] Popular programming languages have MVC frameworks that facilitate the implementation of the pattern.</p>
+<p>Traditionally used for desktop graphical user interfaces (GUIs), this pattern became popular for designing web applications. Popular programming languages have MVC frameworks that facilitate the implementation of the pattern.</p>
+<h2>Model</h2>
+<p>The model is the central component of the pattern. It is the application's dynamic data structure, independent of the user interface. The model is essential for keeping the data organized and consistent.</p>
+
+<h2>View</h2>
+<p>The view is any representation of information such as a chart, diagram or table. Multiple views of the same information are possible, such as a bar chart for management and a tabular view for accountants.</p>
+
+<h2>Controller</h2>
+<p>The controller accepts input and converts it to commands for the model. It then prepares the model data and supplies it to the view.</p>
